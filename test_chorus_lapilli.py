245bd31331b2ad82544d687178e65e9bd2877fb0
Aghilan Nachiappan <aghilannachi@g.ucla.edu>
2026-04-22 21:25:48 -0700

    wrote 3 test cases

--- test_chorus_lapilli.py
+++ test_chorus_lapilli.py
@@ -155,6 +155,37 @@ class TestChorusLapilli(unittest.TestCase):
         tiles[0].click()
         self.assertTileIs(tiles[0], self.SYMBOL_X)
 
+    def test_player_switch(self):
+        '''Check if second move is an O.'''
+        tiles = self.driver.find_elements(By.XPATH, self.BOARD_TILE_XPATH)
+        tiles[0].click()
+        self.assertTileIs(tiles[1], self.SYMBOL_BLANK)
+        tiles[1].click()
+        self.assertTileIs(tiles[1], self.SYMBOL_O)
+
+    def test_player_second_switch(self):
+        '''Check if third move is an X.'''
+        tiles = self.driver.find_elements(By.XPATH, self.BOARD_TILE_XPATH)
+        tiles[0].click()
+        tiles[1].click()
+        self.assertTileIs(tiles[2], self.SYMBOL_BLANK)
+        tiles[2].click()
+        self.assertTileIs(tiles[2], self.SYMBOL_X)
+    
+    def test_no_move_after_win(self):
+        '''Check if moves after win don't work.'''
+        tiles = self.driver.find_elements(By.XPATH, self.BOARD_TILE_XPATH)
+        tiles[0].click()
+        tiles[3].click()
+        tiles[1].click()
+        tiles[4].click()
+        tiles[2].click()
+        self.assertTileIs(tiles[7], self.SYMBOL_BLANK)
+        tiles[7].click()
+        self.assertTileIs(tiles[7], self.SYMBOL_BLANK)
+
+        
+
 
 # ================= [DO NOT MAKE ANY CHANGES BELOW THIS LINE] =================
 
